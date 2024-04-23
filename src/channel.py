b34ca5f2b207507e8c42b37c7454493f70020830
bogdan.kozintsov <bogdan.kozintsov@gmail.com>
2024-04-23 14:06:52 +1100

    added:     @property     def channel_id(self)
        @property
        def title(self):

--- src/channel.py
+++ src/channel.py
@@ -2,6 +2,7 @@ import json
 import os
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
+from pprint import pprint
 import isodate
 
 
@@ -14,10 +15,31 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
 
     def printj(dict_to_print: dict) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
         print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @property
+    def title(self):
+        return self.channel['items'][0]['snippet']['title']
+
+    @property
+    def video_count(self):
+        pass
+
+    @property
+    def url(self):
+        pass
+
+    @classmethod
+    def get_service(cls):
+        pass
 
     '''
     получить данные о канале по его id
@@ -26,9 +48,9 @@ class Channel:
     сервис для быстрого получения id канала: https://commentpicker.com/youtube-channel-id.php
     '''
 
-    channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
-    channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-    printj(channel)
+    # channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+    # channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+    # printj(channel)
 
     '''
     получить данные по видеороликам в плейлисте
@@ -46,4 +68,4 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(self.__channel_id)
+        pprint(self.__channel_id)
